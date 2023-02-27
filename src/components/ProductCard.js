07e09b69d34f08bab30e0a597317389aff021b6b
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-02-27 19:20:35 +0600

    Add initial projects

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,18 +5,17 @@ const ProductCard = ({ product }) => {
   return (
     <div
       className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      key={product._id}
     >
       <div className='h-52 w-52 mx-auto'>
-        <img src={product.image} alt={product.model} />
+        <img src={ product.image } alt={ product.model } />
       </div>
-      <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
+      <h1 className='font-bold text-center'>{ product.model }</h1>
+      <p className='text-center font-semibold mb-3'>Rating: { product.rating }</p>
       <div className=' flex-1'>
         <ul className='space-y-2'>
-          {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
-          })}
+          { product.keyFeature.map((feature, index) => {
+            return <li key={ index } className='text-sm '>{ feature }</li>;
+          }) }
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from 'react';
+import ProductCard from '../components/ProductCard';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data.products))
+
+
+  }, [])
+
+
+
+
+
+
+
+
+
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+      {
+        products.map((product, index) => <ProductCard key={ index } product={ product } />)
+      }
     </div>
   );
 };
